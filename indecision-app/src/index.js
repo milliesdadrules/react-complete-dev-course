da8ee6365d8dc3891c8757252908b2d603fec062
James McNeil <mcneil_james@hotmail.com>
2020-11-27 13:34:57 +0000

    created app object and referenced properties

--- indecision-app/src/index.js
+++ indecision-app/src/index.js
@@ -1,8 +1,14 @@
 // JSX JavaScript XML 
+
+const app = {
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer"
+}
+
 let template = (
     <div>
-        <h1>Indecision App</h1>
-        <p>This is some info</p>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
         <ol>
             <li>Item One</li>
             <li>Item Two</li>
@@ -10,13 +16,21 @@ let template = (
     </div>
 )
 
+const user = {
+    name: 'James McNeil',
+    age: 47,
+    location: 'North Ayrshire'
+}
+const userName = 'James'
+const userAge = 47
+const userLocation = "Scotland"
 let templateTwo = (
     <div>
-        <h1>James McNeil</h1>
-        <p>Age: 47</p>
-        <p>Location: North Ayrshire</p>
+        <h1>{ user.name }</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
     </div>
 )
-let userDetails = document.getElementById('user-details')
+
 let appRoot = document.getElementById('app')
-ReactDOM.render(templateTwo, appRoot)
+ReactDOM.render(template, appRoot)
