1f4ff3f9b70efe0845fc6d66194870544f473556
James McNeil <mcneil_james@hotmail.com>
2020-11-27 14:06:38 +0000

    challenge conditionals

--- indecision-app/src/index.js
+++ indecision-app/src/index.js
@@ -2,13 +2,15 @@
 
 const app = {
     title: "Indecision App",
-    subtitle: "Put your life in the hands of a computer"
+    subtitle: "Put your life in the hands of a computer",
+    options: ['One','Two']
 }
 
-let template = (
+const template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options': 'No Options'}</p>
         <ol>
             <li>Item One</li>
             <li>Item Two</li>
@@ -24,11 +26,20 @@ const user = {
 const userName = 'James'
 const userAge = 47
 const userLocation = "Scotland"
+
+const getLocation = (location) =>{
+    if(location){
+        return <p>Location: {location}</p>
+    } else {
+        return undefined
+    }
+}
+
 let templateTwo = (
     <div>
-        <h1>{ user.name }</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
+        <h1>{ user.name ? user.name: 'Anonymous' }</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 )
 
