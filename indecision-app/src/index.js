db85c7493fd517b3c91c21efbe371344d4216938
James McNeil <mcneil_james@hotmail.com>
2020-11-28 15:00:30 +0000

    added renderCounterApp function

--- indecision-app/src/index.js
+++ indecision-app/src/index.js
@@ -19,18 +19,30 @@ const template = (
 )
 
 let count = 0
-const addOne = () => console.log('addOne');
-const minusOne = () => console.log('minusOne');
-const reset = () => console.log('reset');
-
-const templateTwo = (
-    <div>
-        <h1>Count: {count}</h1>
-        <button onClick={addOne} >+1</button>
-        <button onClick={minusOne}>-1</button>
-        <button onClick={reset}>reset</button>
-    </div>
-)
+const addOne = () => {
+    count++
+    renderCounterApp()
+    }
+const minusOne = () => {
+    count--
+    renderCounterApp()
+}
+const reset = () => {
+    count = 0
+    renderCounterApp()
+}
 
 const appRoot = document.getElementById('app')
-ReactDOM.render(templateTwo, appRoot)
+
+const renderCounterApp = ()=>{
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne} >+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={reset}>reset</button>
+        </div>
+    )
+    ReactDOM.render(templateTwo, appRoot)
+}
+renderCounterApp()
