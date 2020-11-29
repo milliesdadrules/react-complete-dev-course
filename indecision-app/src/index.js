198b7e5421926a40031b214300b0b194d8d8c9e2
James McNeil <mcneil_james@hotmail.com>
2020-11-29 00:21:42 +0000

    added renderApp function

--- indecision-app/src/index.js
+++ indecision-app/src/index.js
@@ -3,25 +3,57 @@
 const app = {
     title: "Indecision App",
     subtitle: "Put your life in the hands of a computer",
-    options: ['One','Two']
+    options: []
 }
 
+const onFormSubmit = (e) => {
+    e.preventDefault()
+    const option = e.target.elements.option.value
+    
+    if(option){
+        app.options.push(option)
+        e.target.elements.option.value = ''
+        renderApp()
+    }
+}
 const template = (
     <div>
         <h1>{app.title}</h1>
         {app.subtitle && <p>{app.subtitle}</p>}
         <p>{app.options.length > 0 ? 'Here are your options': 'No Options'}</p>
+        <p>{app.options.length}</p>
         <ol>
             <li>Item One</li>
             <li>Item Two</li>
         </ol>
-        <form>
+        <form onSubmit={onFormSubmit}>
             <input type="text" name="option"/>
             <button>Add Option</button>
         </form>
     </div>
 )
-
 const appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot)
+const renderApp = () =>{
+
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options': 'No Options'}</p>
+            <p>{app.options.length}</p>
+            <ol>
+                <li>Item One</li>
+                <li>Item Two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+        </div>
+    )
+
+    ReactDOM.render(template, appRoot)
+}
+
+renderApp()
