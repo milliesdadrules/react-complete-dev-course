df8f2c2e7ee48fe4559225d3e2f8a73688f40839
James McNeil <mcneil_james@hotmail.com>
2020-11-29 00:33:01 +0000

    added removeAll function

--- indecision-app/src/index.js
+++ indecision-app/src/index.js
@@ -6,6 +6,10 @@ const app = {
     options: []
 }
 
+const removeAll = ()=> {
+    app.options = []
+    renderApp()
+}
 const onFormSubmit = (e) => {
     e.preventDefault()
     const option = e.target.elements.option.value
@@ -16,22 +20,7 @@ const onFormSubmit = (e) => {
         renderApp()
     }
 }
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'Here are your options': 'No Options'}</p>
-        <p>{app.options.length}</p>
-        <ol>
-            <li>Item One</li>
-            <li>Item Two</li>
-        </ol>
-        <form onSubmit={onFormSubmit}>
-            <input type="text" name="option"/>
-            <button>Add Option</button>
-        </form>
-    </div>
-)
+
 const appRoot = document.getElementById('app')
 
 const renderApp = () =>{
@@ -42,6 +31,7 @@ const renderApp = () =>{
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{app.options.length > 0 ? 'Here are your options': 'No Options'}</p>
             <p>{app.options.length}</p>
+            <button onClick={removeAll}>Remove All</button>
             <ol>
                 <li>Item One</li>
                 <li>Item Two</li>
