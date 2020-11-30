4cacdd57b8d6134badbefaac31333de5fadf4d29
James McNeil <mcneil_james@hotmail.com>
2020-11-30 14:15:22 +0000

    moved old indecision jsx from index.js as ref

---
+++ indecision-app/playground/jsx-indesicion.js
@@ -0,0 +1,57 @@
+// JSX JavaScript XML 
+
+const app = {
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer",
+    options: []
+}
+
+const removeAll = ()=> {
+    app.options = []
+    renderApp()
+}
+
+const onFormSubmit = (e) => {
+    e.preventDefault()
+    const option = e.target.elements.option.value
+    
+    if(option){
+        app.options.push(option)
+        e.target.elements.option.value = ''
+        renderApp()
+    }
+}
+
+const onMakeDecision = () =>{
+    const randonNum = Math.floor(Math.random() * app.options.length)
+    const option = app.options[randonNum]
+    console.log(option);
+}
+
+const appRoot = document.getElementById('app')
+
+const renderApp = () =>{
+
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options': 'No Options'}</p>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+            <button onClick={removeAll}>Remove All</button>
+            <ol>
+            {
+                app.options.map((option, index) => <li key={index}>{option}</li>)
+            }
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+        </div>
+    )
+
+    ReactDOM.render(template, appRoot)
+}
+
+renderApp()
