772cb012ee2c86f309f6ed881c996a30b771da45
James McNeil <mcneil_james@hotmail.com>
2020-12-10 13:37:17 +0000

    import normalize.css

--- indecision-app/src/index.js
+++ indecision-app/src/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
 import IndecisionApp from './components/IndecisionApp'
+import 'normalize.css/normalize.css'
 import './style/main.scss'
 render(<IndecisionApp />, document.getElementById('app'))
