3e4fa4f76f5b31f9f56f876f6e4c5d3178c35ecd
James McNeil <mcneil_james@hotmail.com>
2020-12-12 16:29:11 +0000

    initial commit

---
+++ expensify-app/src/index.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+import 'normalize.css/normalize.css'
+import './style/main.scss'
+render(<p>This is my boilerplate</p>, document.getElementById('app'))
