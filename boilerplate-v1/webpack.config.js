15cd9219bfb815ff9813f118d300cf3abf2ccb4d
James McNeil <mcneil_james@hotmail.com>
2020-12-13 12:50:18 +0000

    initial commit of future project boilerplate

---
+++ boilerplate-v1/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+    entry: ['babel-polyfill','./src/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'public/scripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env','react'],
+                    plugins: ['transform-object-rest-spread','transform-class-properties']
+                }
+            }
+        },{
+            test: /\.s?css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' }
+                ]
+        }]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        publicPath: '/scripts/'
+    },
+    devtool: 'source-map'
+}
