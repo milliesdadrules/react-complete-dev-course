2e8dfe87f04539b4306650a0e0900fe7a4400ea5
James McNeil <mcneil_james@hotmail.com>
2020-12-16 10:43:03 +0000

    added edit and help route

--- expensify-app/src/index.js
+++ expensify-app/src/index.js
@@ -15,11 +15,25 @@ const AddExpensePage = ()=>(
         This is from my add expense component
     </div>
 )
+
+const EditExpensePage = ()=>(
+    <div>
+        This is from my edit expense component
+    </div>
+)
+
+const HelpPage = ()=>(
+    <div>
+        This is from my help component
+    </div>
+)
 const routes = (
     <BrowserRouter>
     <div>
-        <Route path="/" component={ExpenseDashboardPage}/>
+        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
         <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage}/>
     </div>
 
     </BrowserRouter>
