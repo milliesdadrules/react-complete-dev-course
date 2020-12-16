b86d5fa056965a2d6b2303194a7877c6882d40d3
James McNeil <mcneil_james@hotmail.com>
2020-12-16 10:51:06 +0000

    import and use Switch for Routes

--- expensify-app/src/index.js
+++ expensify-app/src/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './style/main.scss'
 
@@ -27,15 +27,22 @@ const HelpPage = ()=>(
         This is from my help component
     </div>
 )
+
+const NotFoundPage = ()=>(
+    <div>
+        404!
+    </div>
+)
+
 const routes = (
     <BrowserRouter>
-    <div>
-        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage}/>
-    </div>
-
+        <Switch>
+            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage}/>
+            <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
 )
 ReactDOM.render(routes, document.getElementById('app'))
