dbc8dabd6e42bc735a6e36095afed40a28389805
James McNeil <mcneil_james@hotmail.com>
2020-12-22 15:52:24 +0000

    imported store, actions selectors and test data

--- expensify-app/src/index.js
+++ expensify-app/src/index.js
@@ -1,8 +1,24 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './style/main.scss'
 
+const store = configureStore()
+
+const unsubscribe = store.subscribe(() => {
+    const state = store.getState() // Get current state
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters) // Create call to function with 2 params expenses array and filters object
+    console.log(visibleExpenses);
+})
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 1000, createdAt: 100}))
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 2000, createdAt: 102}))
+store.dispatch(setTextFilter('bill'))
+store.dispatch(setTextFilter('water'))
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'))
