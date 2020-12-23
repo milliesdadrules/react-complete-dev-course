7715b7d0f5a37ecea11dfb28ca3ccccbe22291ac
James McNeil <mcneil_james@hotmail.com>
2020-12-23 15:42:59 +0000

    import 'Provider' react-redux & breakout AppRouter

--- expensify-app/src/index.js
+++ expensify-app/src/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from "react"
+import ReactDOM from "react-dom"
+import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import { addExpense } from './actions/expenses'
@@ -20,4 +21,11 @@ store.dispatch(addExpense({ description: 'Water Bill', amount: 1000, createdAt: 
 store.dispatch(addExpense({ description: 'Gas Bill', amount: 2000, createdAt: 102}))
 store.dispatch(setTextFilter('water'))
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
