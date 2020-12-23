6ab6dff68832ef583b27cfff42ec948aff76422e
James McNeil <mcneil_james@hotmail.com>
2020-12-23 15:23:25 +0000

    tested hoc useage

--- expensify-app/src/playground/hoc.js
+++ expensify-app/src/playground/hoc.js
@@ -1,1 +1,35 @@
-console.log('working');
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Info = (props) => (
+    <div>
+        <h1>Info</h1>
+        <p>This info is: {props.info}</p>
+    </div>
+)
+
+const withAdminWarning = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAdmin && <p>This is private info. Please don't share!</p>}
+            <WrappedComponent {...props} />
+        </div>
+    )
+}
+
+const requiresAuthentication = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAuthenticated ? (
+                <WrappedComponent {...props} />
+            ) : ( 
+                <p>Please log in to view info!</p>
+            )}
+        </div>
+    )
+}
+const AdminInfo = withAdminWarning(Info)
+const AuthInfo = requiresAuthentication(Info)
+
+//ReactDOM.render(<AdminInfo isAdmin={false} info="These are the details" />, document.getElementById('app'))
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="These are the details" />, document.getElementById('app'))
