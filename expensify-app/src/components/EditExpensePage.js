bdac4bc2b0143968fa399e5cdb60c410a1d568ea
James McNeil <mcneil_james@hotmail.com>
2020-12-29 18:49:36 +0000

    imported connect, ExpenseForm action editExpense

--- expensify-app/src/components/EditExpensePage.js
+++ expensify-app/src/components/EditExpensePage.js
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
+import ExpenseForm from "./ExpenseForm";
+import { editExpense } from '../actions/expenses'
+import { connect } from 'react-redux'
 
-const EditExpensePage = (props)=> {
-    console.log(props);
+const EditExpensePage = ( props )=> {
     return (
         <div>
-            Editing the expense with id of {props.match.params.id}
+            <ExpenseForm 
+                expense={props.expense}
+                onSubmit={(expense) => {
+                    props.dispatch(editExpense(props.match.params.id, expense))
+                    props.history.push('/')
+                }}
+            />
         </div>
     )
 }
+const mapStateToProps = (state, props) =>{
+    return {
+        expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+    }
+} 
 
-export default EditExpensePage
+export default connect(mapStateToProps)(EditExpensePage)
