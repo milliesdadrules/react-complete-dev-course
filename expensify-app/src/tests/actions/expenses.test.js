6f13a26c7120c239fe8ca662aad6756ed1b5a436
James McNeil <mcneil_james@hotmail.com>
2020-12-30 18:23:12 +0000

    added tests for addExpense action generator

--- expensify-app/src/tests/actions/expenses.test.js
+++ expensify-app/src/tests/actions/expenses.test.js
@@ -8,3 +8,45 @@ test('should setup remove expense action object', () => {
     })
 })
 
+test('should setup edit expense action object', () => {
+    const action = editExpense( '123abc', { desciption: 'some info', amount: "10000"})
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '123abc',
+        updates: {
+            desciption: 'some info',
+            amount: '10000'
+        }
+    })
+})
+
+test('should setup add expense action object with provided values', () => {
+    const expenseData = {
+        description: 'Rent',
+        amount: '100000',
+        createdAt: 1000,
+        note: 'This was last months rent'
+    }
+    const action = addExpense(expenseData)
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseData,
+            id: expect.any(String)
+        }
+    })
+})
+
+test('should setup add expense action object with default values', () => {
+    const action = addExpense()
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            description: '', 
+            note: '', 
+            amount: 0, 
+            createdAt: 0,
+            id: expect.any(String)
+        }
+    })
+})
