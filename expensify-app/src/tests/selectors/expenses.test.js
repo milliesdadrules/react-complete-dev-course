0fbea7cf0c7a4ea9c1dd8969863340f6000ebccb
James McNeil <mcneil_james@hotmail.com>
2021-01-01 15:06:04 +0000

    added test for filter by date

---
+++ expensify-app/src/tests/selectors/expenses.test.js
@@ -0,0 +1,34 @@
+import selectExpenses from '../../selectors/expenses'
+
+const expenses = [{
+    id: '1',
+    description: 'gum',
+    note: '',
+    amount: '195',
+    createdAt: 0,
+  }, {
+    id: '2',
+    description: 'Rent',
+    note: '',
+    amount: '10950',
+    createdAt: -1000,
+  }, {
+    id: '3',
+    description: 'Credit Card',
+    note: '',
+    amount: '4500',
+    createdAt: 1000,
+  }]
+
+test('should filter text by value', () => {
+  const filters = {
+    text: 'e',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+  }
+  const result = selectExpenses(expenses, filters)
+
+  expect(result).toEqual([expenses[2], expenses[1]])
+
+})
