4a8b29738b3cb49a0b0da148060619b89684a509
James McNeil <mcneil_james@hotmail.com>
2021-01-11 21:29:49 +0000

    added tests for remaining reducers

--- expensify-app/src/tests/reducers/filters.test.js
+++ expensify-app/src/tests/reducers/filters.test.js
@@ -1,12 +1,56 @@
 import moment from 'moment'
-import filterReducer from '../../reducers/filters'
+import filtersReducer from '../../reducers/filters'
 
 test('should setup default filter values', () => {
-  const state = filterReducer(undefined, '@@INIT')
+  const state = filtersReducer(undefined, '@@INIT')
   expect(state).toEqual({
     text: '',
     sortBy: 'date',
     startDate: moment().startOf('month'),
     endDate: moment().endOf('month'),
   })
+})
+
+test('should set sortBy to amount', () => {
+  const state = filtersReducer(undefined, { type: 'SORT_BY_AMOUNT' })
+  expect(state.sortBy).toBe('amount')
+})
+
+test('should set sortBy to date', () => {
+  const currentState = {
+    text: '',
+    sortBy: 'amount',
+    startDate: moment().startOf('month'),
+    endDate: moment().endOf('month'),
+  }
+  const action = { type: 'SORT_BY_DATE' }
+  const state = filtersReducer(currentState, action)
+  expect(state.sortBy).toBe('date')
+})
+
+// test('should set text filter')
+test('should set text filter', () => {
+  const state = filtersReducer(undefined, {
+    type: 'SET_TEXT_FILTER',
+    text: 'rent',
+  })
+  expect(state.text).toBe('rent')
+})
+
+// test('should set startDate filter')
+test('should set startDate filter', () => {
+  const state = filtersReducer(undefined, {
+    type: 'SET_START_DATE',
+    startDate: moment().startOf('month'),
+  })
+  expect(state.startDate).toEqual(moment().startOf('month'))
+})
+
+// test('should set endDate filter')
+test('should set endtDate filter', () => {
+  const state = filtersReducer(undefined, {
+    type: 'SET_END_DATE',
+    endDate: moment().endOf('month'),
+  })
+  expect(state.endDate).toEqual(moment().endOf('month'))
 })
