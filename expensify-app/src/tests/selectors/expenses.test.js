181cceecc75b46c272497d7f5fa054eeb4a30e48
James McNeil <mcneil_james@hotmail.com>
2021-01-11 21:50:49 +0000

    removed test expenses array added to fixture file

--- expensify-app/src/tests/selectors/expenses.test.js
+++ expensify-app/src/tests/selectors/expenses.test.js
@@ -1,29 +1,6 @@
 import selectExpenses from '../../selectors/expenses'
 import moment from 'moment'
-
-const expenses = [
-  {
-    id: '1',
-    description: 'gum',
-    note: '',
-    amount: '100',
-    createdAt: 0,
-  },
-  {
-    id: '2',
-    description: 'Rent',
-    note: '',
-    amount: '300',
-    createdAt: moment(0).subtract(4, 'days').valueOf(),
-  },
-  {
-    id: '3',
-    description: 'Credit Card',
-    note: '',
-    amount: '200',
-    createdAt: moment(0).add(4, 'days').valueOf(),
-  },
-]
+import expenses from '../fixtures/expenses'
 
 test('should filter text by value', () => {
   const filters = {
