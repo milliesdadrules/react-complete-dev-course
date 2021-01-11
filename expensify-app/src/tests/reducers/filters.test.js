ab3d97bf4f13df86052fce25459480554b0a7271
James McNeil <mcneil_james@hotmail.com>
2021-01-11 20:54:59 +0000

    added test for default filters

--- expensify-app/src/tests/reducers/filters.test.js
+++ expensify-app/src/tests/reducers/filters.test.js
@@ -1,1 +1,12 @@
-import filterReducers from '../../reducers/filters'
+import moment from 'moment'
+import filterReducer from '../../reducers/filters'
+
+test('should setup default filter values', () => {
+  const state = filterReducer(undefined, '@@INIT')
+  expect(state).toEqual({
+    text: '',
+    sortBy: 'date',
+    startDate: moment().startOf('month'),
+    endDate: moment().endOf('month'),
+  })
+})
