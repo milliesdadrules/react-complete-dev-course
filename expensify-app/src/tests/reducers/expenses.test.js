3b30dc54c3bfe83346972a11e9d4b54764a32a17
James McNeil <mcneil_james@hotmail.com>
2021-01-12 19:51:33 +0000

    added test to add and update expenses

--- expensify-app/src/tests/reducers/expenses.test.js
+++ expensify-app/src/tests/reducers/expenses.test.js
@@ -24,7 +24,43 @@ test('should not remove expense if id not found', () => {
   expect(state).toEqual([expenses[0], expenses[1], expenses[2]])
 })
 
-// test('should add expense')
+test('should add expense', () => {
+  const action = {
+    type: 'ADD_EXPENSE',
+  }
+  const expense = {
+    id: '4',
+    description: 'Gum',
+    note: 'Test expense',
+    amount: '100',
+    createdAt: 0,
+  }
+  const state = expensesReducer(expenses, action)
+  expect(state).toEqual([...expenses, expenses[3]])
+})
 
-// test('should edit expense')
-// test('should NOT edit expense if id not found')
+test('should edit expense', () => {
+  const amount = '20'
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: expenses[1].id,
+    updates: {
+      amount,
+    },
+  }
+  const state = expensesReducer(expenses, action)
+  expect(state[1].amount).toBe(amount)
+})
+
+test('should Not edit expense if id not found', () => {
+  const amount = '20'
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: 10,
+    updates: {
+      amount,
+    },
+  }
+  const state = expensesReducer(expenses, action)
+  expect(state).toEqual(expenses)
+})
