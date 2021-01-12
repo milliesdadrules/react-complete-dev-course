8e91b19e1dab6b1322d82d04485eb570d8805974
James McNeil <mcneil_james@hotmail.com>
2021-01-12 18:58:06 +0000

    added test for removing expenses

--- expensify-app/src/tests/reducers/expenses.test.js
+++ expensify-app/src/tests/reducers/expenses.test.js
@@ -5,3 +5,26 @@ test('should set default state', () => {
   const state = expensesReducer(undefined, { type: '@@INIT' })
   expect(state).toEqual([])
 })
+
+test('should remove expense by id', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: expenses[1].id,
+  }
+  const state = expensesReducer(expenses, action)
+  expect(state).toEqual([expenses[0], expenses[2]])
+})
+
+test('should not remove expense if id not found', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: '5',
+  }
+  const state = expensesReducer(expenses, action)
+  expect(state).toEqual([expenses[0], expenses[1], expenses[2]])
+})
+
+// test('should add expense')
+
+// test('should edit expense')
+// test('should NOT edit expense if id not found')
